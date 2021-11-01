47bed79f290a4495360202d5c4b366f28fef0635
VanDeGraf <vandegraf@bk.ru>
2021-11-01 06:47:37 +0400

    Config update

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: "devise/sessions#new"
-  devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   get "users/auth/:provider/callback", to: "users/omniauth_callbacks#entry"
 
   devise_scope :user do
